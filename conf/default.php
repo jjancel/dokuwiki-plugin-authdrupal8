9f012780ac5102268f438c2ea53ba2f4bcd03b28
jjancel <75393560+jjancel@users.noreply.github.com>
2020-12-02 21:46:12 +0100

    Create default.php

---
+++ conf/default.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Default settings for the authdrupal8 plugin
+ *
+ * @author jjancel <jjancel@dialoguesenhumanite.org>
+ */
+
+$conf['database']	= 'jjancel_dru8tst';
+$conf['username']	= 'root';
+$conf['password']	= 'root';
+$conf['prefix']		= '';
+$conf['host']			= 'localhost';
+$conf['debug']		= 2;
